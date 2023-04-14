8f7fdef8ed1887f7b76007bac4de12622c0833af
Osomasi <osomasi19@gmail.com>
2023-04-14 18:20:57 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import Home from './Pages/home'
 import SignUp from './Pages/signup'
+import Login from './Pages/login';
 import Error from './Pages/404'
 import { Route, Routes, Link } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path = "/carbon-footprint-calculator/" element= { <Home /> } key = { document.location.href } />
         <Route path = "/carbon-footprint-calculator/signup" element= { <SignUp /> } key = { document.location.href } />
+        <Route path = "/carbon-footprint-calculator/login" element= { <Login /> } key = { document.location.href } />
         <Route path = "*" element= { <Error /> } key = { document.location.href } />
       </Routes>
     </div>
