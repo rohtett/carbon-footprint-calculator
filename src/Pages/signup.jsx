15f5433e9a84b96df5ce180888658b099b3f80a5
Rohit <raheet@hotmail.co.uk>
2023-04-17 14:48:56 +0100

    Styles to the signup form

--- src/Pages/signup.jsx
+++ src/Pages/signup.jsx
@@ -4,23 +4,59 @@ import '../App.scss';
 const signup = () => {
   return (
     <div className="container">
-      <div className="separator">
+      <div className="separator backdrop">
       </div>
       <div className="banner">
-        <form className="sign-up-form">
-          <label className="sign-up-form__label" htmlFor="name">Name</label>
-          <input className="sign-up-form__input" type="text" name="name" id="name" />
-
-          <label className="sign-up-form__label" htmlFor="email">Email</label>
-          <input className="sign-up-form__input" type="email" name="email" id="email" />
-
-          <label className="sign-up-form__label" htmlFor="password">Password</label>
-          <input className="sign-up-form__input" type="password" name="password" id="password" />
-
-          <label className="sign-up-form__label" htmlFor="confirm-password">Confirm Password</label>
-          <input className="sign-up-form__input" type="password" name="confirm-password" id="confirm-password" />
-
-          <button className="sign-up-form__button" type="submit">Sign Up</button>
+        <form className="signup__form">
+          <div className="signup__form__item">
+            <input className="signup__form__item--input"
+              type="text" name="name" id="name"
+              onChange={(e) => {
+                if (e.target.value) {
+                  e.target.classList.add("signup__form__item--input--contains");
+                } else {
+                  e.target.classList.remove("signup__form__item--input--contains")
+                }
+              }}/>
+            <label className="signup__form__item--label" htmlFor="name">Name</label>
+          </div>
+          <div className="signup__form__item">
+            <input className="signup__form__item--input"
+              type="email" name="email" id="email"
+              onChange={(e) => {
+                if (e.target.value) {
+                  e.target.classList.add("signup__form__item--input--contains");
+                } else {
+                  e.target.classList.remove("signup__form__item--input--contains")
+                }
+              }}/>
+            <label className="signup__form__item--label" htmlFor="email">Email</label>
+          </div>
+          <div className="signup__form__item">
+            <input className="signup__form__item--input"
+              type="password" name="password" id="password"
+              onChange={(e) => {
+                if (e.target.value) {
+                  e.target.classList.add("signup__form__item--input--contains");
+                } else {
+                  e.target.classList.remove("signup__form__item--input--contains")
+                }
+              }}/>
+            <label className="signup__form__item--label" htmlFor="password">Password</label>
+          </div>
+          <div className="signup__form__item">
+            <input className="signup__form__item--input"
+              type="password" name="confirm-password"
+              id="confirm-password" onChange={(e) => {
+                if (e.target.value) {
+                  e.target.classList.add("signup__form__item--input--contains");
+                } else {
+                  e.target.classList.remove("signup__form__item--input--contains")
+                }
+              }}/>
+            <label className="signup__form__item--label" htmlFor="confirm-password">Confirm Password</label>
+          </div>
+          <button className="signup__form__button navigators__link--white" type="submit">Sign Up</button>
         </form>
       </div>
     </div>
